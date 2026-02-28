8c550981c63fdb1471792adcc79074095376b95d
Chris McIntosh <chrismcintosh@Mac.lan>
2026-02-28 10:15:24 -0600

    S-013: Define CTI Settings → Voicemail relationship

---
+++ src/custom/Extension/modules/SweetDialerCTI/Ext/Language/en_us.voice_mail.php
@@ -0,0 +1,5 @@
+<?php
+$mod_strings['LBL_TWILIO_VOICE_MAIL'] = 'Default Voicemail';
+$mod_strings['LBL_TWILIO_VOICE_MAIL_ID'] = 'Default Voicemail ID';
+$mod_strings['LBL_VOICEMAILS'] = 'Voicemails';
+$mod_strings['LBL_VOICEMAILS_SUBPANEL_TITLE'] = 'Voicemail Messages';
