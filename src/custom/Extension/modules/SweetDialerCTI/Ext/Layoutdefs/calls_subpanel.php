a5f8ff406f8fb339d7932303ce3251ba3b65ea4b
Chris McIntosh <chrismcintosh@Mac.lan>
2026-02-28 10:16:37 -0600

    S-014: Define CTI Calls → CTI Settings relationship

---
+++ src/custom/Extension/modules/SweetDialerCTI/Ext/Layoutdefs/calls_subpanel.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * S-014: CTI Settings -> Calls Subpanel
+ */
+$layout_defs['SweetDialerCTI']['subpanel_setup']['calls'] = array(
+    'order' => 40,
+    'sort_order' => 'desc',
+    'sort_by' => 'date_entered',
+    'module' => 'SweetDialerCalls',
+    'subpanel_name' => 'default',
+    'title_key' => 'LBL_CALLS_SUBPANEL_TITLE',
+);
