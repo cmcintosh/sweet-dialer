1ac82d7a796515768902e3f00c7bb1d7ec234ca3
Chris McIntosh <cmcintosh@wembassy.com>
2026-02-28 20:30:41 -0600

    Fix manifest: target SugarCRM 6.5 and 8.x versions per SuiteCRM docs

--- src/manifest.php
+++ src/manifest.php
@@ -1,31 +1,36 @@
 <?php
-// SuiteCRM 8.8.0 Module Manifest
+/**
+ * SweetDialer Module Manifest
+ */
 
 $manifest = array(
     'name' => 'SweetDialer',
-    'label' => 'SweetDialer',
     'description' => 'Twilio AI-Powered Dialer for SuiteCRM',
     'version' => '1.0.0',
     'author' => 'Wembassy',
-    'is_uninstallable' => true,
     'type' => 'module',
-    'acceptable_sugar_versions' => array(
-        'exact_matches' => array('8.8.0', '8.8.1', '8.8.2', '8.8.3', '8.8.4'),
-        'regex_matches' => array('8\.[0-9]+\.[0-9]+', '7\.[0-9]+\.[0-9]+'),
-    ),
+    'is_uninstallable' => true,
     'acceptable_sugar_flavors' => array('CE'),
+    'acceptable_sugar_versions' => array(
+        'exact_matches' => array(),
+        'regex_matches' => array('6\\.5\\.[0-9]+', '8\\.[0-9]+\\.[0-9]+'),
+    ),
 );
 
 $installdefs = array(
     'id' => 'SweetDialer',
-    'beans' => array(),
     'copy' => array(
-        array('from' => '<basedir>/custom', 'to' => 'custom'),
-        array('from' => '<basedir>/modules', 'to' => 'modules'),
+        array(
+            'from' => '<basedir>/custom',
+            'to' => 'custom',
+        ),
+        array(
+            'from' => '<basedir>/modules',
+            'to' => 'modules',
+        ),
+        array(
+            'from' => '<basedir>/config',
+            'to' => 'config',
+        ),
     ),
-);
-
-$config = array(
-    'name' => 'SweetDialer',
-    'version' => '1.0.0',
 );
