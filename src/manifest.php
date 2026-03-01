d9f8d188a06ac011be074f602b52cacbfb789723
Chris McIntosh <cmcintosh@wembassy.com>
2026-02-28 20:25:29 -0600

    Complete manifest with beans, copy, language, entrypoints for SuiteCRM 8.8.0

--- src/manifest.php
+++ src/manifest.php
@@ -1,5 +1,4 @@
 <?php
-// SuiteCRM 8.8.0 Compatible Module Manifest
 $manifest = array(
     'name' => 'SweetDialer',
     'description' => 'Twilio AI-Powered Dialer for SuiteCRM',
@@ -7,26 +6,32 @@ $manifest = array(
     'author' => 'Wembassy',
     'type' => 'module',
     'is_uninstallable' => true,
+    'published_date' => '2026-03-01',
+    'readme' => '',
     'acceptable_sugar_versions' => array(
-        'regex_matches' => array(
-            '8\.8\.\d+',
-            '8\.\d+\.\d+',
-            '7\.\d+\.\d+',
-        ),
+        'regex_matches' => array('8\.8\.\d+', '8\.\d+\.\d+', '7\.\d+\.\d+'),
     ),
-    'acceptable_sugar_flavors' => array('CE', 'PRO', 'ENT', 'ULT'),
+    'acceptable_sugar_flavors' => array('CE', 'ENT', 'ULT'),
 );
 
 $installdefs = array(
     'id' => 'SweetDialer',
+    'beans' => array(
+        array(
+            'module' => 'SweetDialerCalls',
+            'class' => 'SweetDialerCalls',
+            'path' => 'modules/SweetDialerCalls/SweetDialerCalls.php',
+            'tab' => true,
+        ),
+    ),
     'copy' => array(
-        array(
-            'from' => '<basedir>/custom',
-            'to' => 'custom',
-        ),
-        array(
-            'from' => '<basedir>/modules',
-            'to' => 'modules',
-        ),
+        array('from' => '<basedir>/custom', 'to' => 'custom'),
+        array('from' => '<basedir>/modules', 'to' => 'modules'),
+    ),
+    'language' => array(
+        array('from' => '<basedir>/custom', 'to' => 'custom'),
+    ),
+    'entrypoints' => array(
+        array('from' => '<basedir>/custom/entrypoints/voiceWebhook.php', 'to' => 'custom/entrypoints/voiceWebhook.php'),
     ),
 );
