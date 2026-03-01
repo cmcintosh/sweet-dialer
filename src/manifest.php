dc80127273d3627d8a03343922a154200b7975d2
Chris McIntosh <cmcintosh@wembassy.com>
2026-02-28 20:20:04 -0600

    Fix manifest version regex for SuiteCRM 8.8.0 compatibility

--- src/manifest.php
+++ src/manifest.php
@@ -1,37 +1,34 @@
 <?php
-/**
- * SweetDialer Manifest - SuiteCRM 8.8.0 Compatible
- */
-
-$manifest = array(
+// SuiteCRM 8.8.0 Compatible Module Manifest
+$manifest = [
     'name' => 'SweetDialer',
-    'description' => 'Twilio AI-Powered Dialer for SuiteCRM 8.x',
+    'description' => 'Twilio AI-Powered Dialer for SuiteCRM',
     'version' => '1.0.0',
     'author' => 'Wembassy',
-    'is_uninstallable' => true,
     'type' => 'module',
-    'published_date' => '2026-03-01',
-    'acceptable_sugar_versions' => array(
-        'regex_matches' => array(
-            '8\.8\.\d+',
-            '8\.\d+\.\d+',
-            '7\.\d+\.\d+',
-        ),
-    ),
-    'acceptable_sugar_flavors' => array(
-        'CE',
-        'ENT',
-        'ULT',
-    ),
-);
+    'is_uninstallable' => true,
+    'built_in_version' => '7.10.0',
+    'acceptable_sugar_versions' => [
+        'exact_matches' => [],
+        'regex_matches' => [
+            '8\\.8\\..*',
+            '8\\..*',
+            '7\\..*',
+        ],
+    ],
+    'acceptable_sugar_flavors' => ['CE', 'PRO', 'ENT', 'ULT'],
+];
 
-$installdefs = array(
+$installdefs = [
     'id' => 'SweetDialer',
-    'module' => 'SweetDialer',
-    'built_in_version' => '1.0.0',
-    'copy' => array(
-        array('from' => '<basedir>/custom', 'to' => 'custom'),
-        array('from' => '<basedir>/modules', 'to' => 'modules'),
-        array('from' => '<basedir>/config', 'to' => 'config'),
-    ),
-);
+    'copy' => [
+        [
+            'from' => '<basedir>/custom',
+            'to' => 'custom',
+        ],
+        [
+            'from' => '<basedir>/modules',
+            'to' => 'modules',
+        ],
+    ],
+];
